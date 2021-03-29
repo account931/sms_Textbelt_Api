de2d34a9b1f6714d891e7abf83bdc655f9d0bb96
Account931 <account931@ukr.net>
2021-03-29 13:38:53 +0300

    Fix(fixed tabs): added .editorconfig to fix Github tab issue

--- Credentials/credentials.php
+++ Credentials/credentials.php
@@ -1,5 +1,5 @@
 <?php
 //Credentials for SMS API
 
-define ("SMS_API_KEY", "textbelt"); //"textbelt" //"textbelt_test"
+define ("SMS_API_KEY", "textbelt"); //"textbelt" => to send 1 real sms //"textbelt_test" => to emulate sending for testing
 ?>
